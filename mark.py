751f8db5aba65e4f4535527c0095c4d89f1e01ac
hemant17 <hemantbolashetti77@gmail.com>
2025-11-27 14:15:07 +0530

    Update mark.py

--- mark.py
+++ mark.py
@@ -1,7 +1,7 @@
 import sys
 if len(sys.argv) !=6:
       print("Usage: python mark.py <sub1> <sub2>")
-    sys.exit(1)
+      sys.exit(1)
 
 script_name = sys.argv[0]
 sub1 = sys.argv[1]
@@ -9,6 +9,14 @@ sub2= sys.argv[2]
 sub3 = sys.argv[3]
 sub4 = sys.argv[4]
 sub5 = sys.argv[5]
+else:
+sub1 = 45
+sub2= 56
+sub3 = 45
+sub4 = 56
+sub5 =57
+
+
 avg=(sub1+sub2+sub3+sub4+sub5)/5
 print("Average marks",avg)
 print("Grade")
